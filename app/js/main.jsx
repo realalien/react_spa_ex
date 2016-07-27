b0c615b6dc00ece85a7c946f437dffb82bc90f9d
realalien <realalien@gmail.com>
2016-07-27 18:22:55 +0800

    replaced asset; put shake page into form

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -20,7 +20,8 @@ var Shake = require('shake.js');
 // TODO:
 // * TODO: the asset of tile icon should be the same size,
 //         otherwise the positions of itself and siblings are too complex
-
+// * TODO: refactor the LogoAndName components to base layout component rather
+//         than on every page component.
 
 var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
@@ -291,6 +292,7 @@ var RaceChoosePage = React.createClass({
       borderRadius : 5,
       backgroundColor: "rgba(218, 33, 39, 1)",
       textAlign : "center",
+      color : "white",
       position : "absolute",
       top : 70,
       marginTop: "12%",
@@ -337,6 +339,219 @@ var RaceChoosePage = React.createClass({
   }
 });
 
+////////////////////////////////////////////////////////
+//
+// 4 HorseSelectPage , shake to get random horse (designed by Are)
+//
+////////////////////////////////////////////////////////
+var HorseSelectPage = React.createClass({
+
+  getInitialState : function() {
+    var myShakeEvent = new Shake({
+        threshold: 15, // optional shake strength threshold
+        timeout: 1000 // optional, determines the frequency of event generation
+    });
+
+    return {
+        event: myShakeEvent,
+        hasShaked: false,
+        hasSelectedHorse: false,  // NOTE: watch out for shaked but no horse selected! selection interrupted by componentUnmounting
+        raceId : ""
+    };
+  },
+
+  shakeEventDidOccur: function() {
+    //put your own code here etc.
+    console.log("Shake detected");
+    alert("shaked!");
+    if (this.state.hasShaked === true) {
+      console.log("already shaked");
+      // TODO: prevent mingling events/activities
+
+    } else {
+      console.log("first shake, won't happen later!");
+      this.setState({hasShaked: true}); // default should not shake
+    }
+  },
+
+  checkShakeState : function(){
+    // test if we can read the state
+    if ( this.state.hasShaked === true) {
+      console.log("checkShakeState, has Shaked  :  true ");
+    } else {
+      console.log("checkShakeState, has Shaked  :  false ");
+    }
+  },
+
+  saveHorseSelection : function(horseId) {
+    console.log("------------   saveHorseSelection ------------ ");
+    // load from storage
+    var racesDidSelected = JSON.parse(localStorage.getItem('racesDidSelected')) || {}
+    console.log("checking racesDidSelected : " + JSON.stringify(racesDidSelected));
+
+    // add new entry
+    racesDidSelected[this.state.raceId] = horseId;
+
+    // save back to storeage
+    localStorage.setItem('racesDidSelected', JSON.stringify(racesDidSelected));
+    // TODO: temp code, check
+    var check = JSON.parse(localStorage.getItem('racesDidSelected')) || {}
+    console.log("in handleHorseBtn, check selection in storage: " + check);
+  },
+
+  getSelectedHorse : function(){
+    var racesDidSelected = JSON.parse(localStorage.getItem('racesDidSelected')) || {};
+    return racesDidSelected[this.state.raceId];
+  },
+
+  componentWillMount: function() {
+     // SUG: make sure all the states are correct before rendering
+     console.log("------------   componentWillMount ------------ ");
+    //  this.checkShakeState();
+
+    // check if user already shaked(selected a target horse for the race) or not.
+    // load from storage
+    var racesDidSelected = JSON.parse(localStorage.getItem('racesDidSelected')) || {}
+    console.log("checking racesDidSelected : " + JSON.stringify(racesDidSelected));
+
+
+    // update hasShaked property and raceId (from params)
+    var id = this.props.params.raceId;
+    console.log( "checking raceId from params: " + this.props.params.raceId );
+    this.setState({raceId: id});
+
+    // TODO: should check if the raceId is permitted, server check!
+
+
+    var idInString = id.toString();
+    if ( idInString in racesDidSelected && racesDidSelected[idInString] !== "" ){
+      this.setState({hasShaked : true, hasSelectedHorse : true});
+      console.log("[INFO] seems has selected horse before");
+    } else {
+      this.setState({hasShaked : false, hasSelectedHorse : false });
+      console.log("[INFO] seems has NOT select horse before");
+    }
+
+    // e.g. if user already shake for the specific race,
+    //      then, no shake event should be mounted
+    //      and,  shaking hands icons should not be displayed.
+    if ( this.state.hasShaked === false) {
+     window.addEventListener('shake', this.shakeEventDidOccur, false);
+     console.log("Shake mounted ");
+    }
+
+  },
+
+  componentDidMount: function () {
+    console.log("------------   componentDidMount ------------ ");
+
+    this.state.event.start();
+    console.log("Shake gadget started");
+  },
+
+  componentWillUnmount: function() {
+    this.state.event.stop();
+
+    window.removeEventListener('shake', this.shakeEventDidOccur, false);
+    console.log("Shake UN mounted ");
+  },
+
+  handleHorseBtn : function(i, props) {
+    console.log(" ------------   handleHorseBtn   --------------  "+i);
+
+    this.setState({hasSelectedHorse : true});
+    this.saveHorseSelection(i);
+  },
+
+  clickForShakeDemo : function() {
+    console.log(" ------------   clickForShakeDemo   -------------- called ");
+    this.shakeEventDidOccur();
+  },
+
+  render : function() {
+    console.log("in render ......");
+    var symbolShakeNode = "";
+    var horsesForSelectionNode = "";
+    if ( this.state.hasShaked === false ) {  // render shake symobol
+      var divStyle = {
+        textAlign : "center",
+        // border : "2px dashed green"
+      }
+      var textStyle = {
+        color : "white",
+        fontSize : 28,
+        fontFamily : "Times New Roman, Times, serif",
+      };
+
+      var imgStyle = {
+        display: "block",
+        margin: "auto",
+        marginTop: 20,
+        marginBottom : 20,
+        width: "30%"
+      };
+
+      symbolShakeNode = (
+        <div style={divStyle}>
+          <img style={imgStyle} onClick={this.clickForShakeDemo} src="../image/shake.png"></img>
+          <p style={textStyle}>Shake to receive</p>
+          <p style={textStyle}>your selection!</p>
+        </div>
+      );
+    } else { // has shaked
+
+      if ( this.state.hasSelectedHorse === true) { // render selection anim or process
+        var textStyle = {
+          color : "white",
+          fontSize : 28,
+          fontFamily : "Times New Roman, Times, serif"
+        } ; // TODO: the code is unreadable!
+        var notice = "You already selected horse! Her name is " + this.getSelectedHorse()
+        horsesForSelectionNode = (
+          <div style={divStyle}>
+            <p style={textStyle}>{notice}</p>
+          </div>
+        );
+      } else { // shaked but not done random selection
+
+        // create array of button for selection, and save the btn index
+        var horses = [];
+
+        var divStyle = {
+          display: "inline",
+          marginTop : 30
+        }
+
+        for (var i=1; i<= 20; i++){
+          var str = "Horse #"+i;
+          horses.push(
+            <div style={divStyle} onClick={this.handleHorseBtn.bind(this, i, this.props)} key={i} >
+              <HorseBtn text={str} value={i}/>
+            </div>
+          );
+        }
+
+        horsesForSelectionNode = (
+           <div>{horses}</div>
+        );
+      }
+    }
+
+    var strForTextBelt =  "Race #" + this.state.raceId;
+    return (
+      <div>
+        <LogoAndName />
+        <TextBelt text={strForTextBelt} />
+        <div></div>
+        {symbolShakeNode}
+        {horsesForSelectionNode}
+      </div>
+
+    );
+  }
+});
+
+
 
 var TextBelt = React.createClass({
   render : function() {
@@ -345,6 +560,7 @@ var TextBelt = React.createClass({
       color: "white",
       backgroundColor: "rgba(0,0,0,0.2)",
       fontSize : 28,
+      fontFamily : "Times New Roman, Times, serif",
       height: 40,
       marginBottom: 30,
       display: "table",
@@ -407,7 +623,9 @@ var RaceSelectTable = React.createClass({
     for (var i=1; i <= this.props.numOfCells; i++) {
       cells.push(
         <div style={divStyle} onClick={this.handleRaceTile.bind(this, i, this.props)} key={i} >
-          <RaceSelectTile width={w} text={i}/>
+          <Link to={"/race/" + i }>
+            <RaceSelectTile width={w} text={i}/>
+          </Link>
         </div>
       )
     }
@@ -650,7 +868,7 @@ var RaceDetailPage = React.createClass({
       var racesAlreadyDoneSelection = this.state.racesDidSelected;
       racesAlreadyDoneSelection[this.state.raceId] = i;
 
-
+      // read localstorage example
       console.log("JSON output: "+ JSON.stringify(racesAlreadyDoneSelection));
       // save to localstorage
       localStorage.setItem('racesDidSelected', JSON.stringify(racesAlreadyDoneSelection));
@@ -1077,6 +1295,7 @@ ReactDOM.render(
       <Route path="/prizes" component={PrizesPage} />
       <Route path="/intro" component={GameIntro} />
       <Route path="/races" component={RaceChoosePage} />
+      <Route path="/race/:raceId" component={HorseSelectPage} />
     </Route>
   </Router>,
   document.querySelector("#container")
